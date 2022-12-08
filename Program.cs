355a9ce9153a3e4a75e890fb5acc34ba02f982f0
Crist <Crist@DESKTOP-RP46J8U>
2022-12-07 21:20:31 -0800

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cristian Enrique Ceballos Ramirez");
+            Console.ReadKey();
+            Console.WriteLine("Mexicali, Baja California");
+            Console.ReadKey();
+            Console.WriteLine("21");
+            Console.ReadKey();
+        }
+    }
+}
